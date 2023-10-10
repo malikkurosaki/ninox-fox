2cf77798b734e8455f35c2e8cc8f39aa497346d3
amel <amel@amel.local>
2023-10-10 11:43:12 +0800

    inisiasi awal
    Deskripsi:
    - Membuat folder modules
    - Membuat routing awal
    
    No Issues

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Group, Title, Image, BackgroundImage } from '@mantine/core'
-
-export default function Home() {
-  return (
-    <BackgroundImage w={"100%"} h={"100vh"} src='/ninox.png'/>
-  )
-}
