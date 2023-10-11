b5fe278301cae16b2df2e896597236f232b7048f
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-11 17:41:05 +0800

    style: tambah pairing
    Deskripsi:
    - menambahkan components modal
    - menambahkan components pairing
    - menambahkan components table data pairing
    - menambahkan components upload data pairing
    - menambahkan view pairing
    - menambahkan view upload pairing
    
    No Issue

---
+++ src/modules/pairing/back/components/table_data_pairing.tsx
@@ -0,0 +1,127 @@
+import { Avatar, Box, Group, Image, Table } from "@mantine/core";
+import React from "react";
+
+const data = [
+  {
+    id: 1,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 2,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 3,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 4,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 5,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 6,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 7,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 8,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 9,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+  {
+    id: 10,
+    candidate1: "I Wayan Adi",
+    candidate2: "I Komang Ayu",
+    Confidence: 22221,
+    Kabupaten: 12123,
+    Supportive: 12121,
+  },
+];
+/**
+ * Fungsi menampilkan halaman table data pairing.
+ * @returns  Hasil dari table data pairing untuk menampilkan data table
+ */
+export default function TableDataPairing() {
+  return (
+    <>
+      <Box
+        style={{
+          backgroundColor: "white",
+          padding: 20,
+          borderRadius: "5px",
+        }}
+      >
+        <Table withTableBorder horizontalSpacing="xl">
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>ID</Table.Th>
+              <Table.Th>CANDIDATE 1</Table.Th>
+              <Table.Th>CANDIDATE 2</Table.Th>
+              <Table.Th>KABUPATEN ID</Table.Th>
+              <Table.Th>CONFIDENCE</Table.Th>
+              <Table.Th>SUPPORTIVE</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {data.map((v, i) => (
+              <Table.Tr key={i}>
+                <Table.Td>{v.id}</Table.Td>
+                <Table.Td>{v.candidate1}</Table.Td>
+                <Table.Td>{v.candidate2}</Table.Td>
+                <Table.Td>{v.Kabupaten}</Table.Td>
+                <Table.Td>{v.Confidence}</Table.Td>
+                <Table.Td>{v.Supportive}</Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Box>
+    </>
+  );
+}
---
+++ src/modules/pairing/back/index.ts
@@ -0,0 +1,5 @@
+import ViewPairing from "./view/view_pairing";
+import ViewUploadPairing from "./view/view_upload_pairing";
+
+export {ViewPairing}
+export {ViewUploadPairing}
