2433e0ef890634e65eb126424efaa35d0721de7e
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-12 17:36:44 +0800

    style: update dahsboard
    Deskripsi:
    - menambahkan copy data
    - update region view list data
    
    No Issue

--- src/modules/popularity/back/index.ts
+++
@@ -1,7 +0,0 @@
-import ViewPopularity from "./view/view_popularity";
-import ViewCopyDataPopularity from "./view/view_copy_data_popularity";
-import ViewUploadDataPopularity from "./view/view_upload_data_popularity";
-
-export {ViewPopularity}
-export {ViewCopyDataPopularity}
-export {ViewUploadDataPopularity}
--- src/modules/popularity/back/view/view_upload_data_popularity.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import UploadDataPopularity from "../components/upload_data_popularity";
-
-export default function ViewUploadDataPopularity() {
-  return <UploadDataPopularity />;
-}
