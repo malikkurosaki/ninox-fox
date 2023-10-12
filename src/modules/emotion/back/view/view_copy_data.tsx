77bab2aa6201ec621ffd9246fbdd78277dcbf15f
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-12 17:25:52 +0800

    style: update emotion
    Deskripsi:
    - update view back emotion
    - update copy data
    
    No Issue

--- src/modules/emotion/back/view/view_copy_data.tsx
+++ src/modules/emotion/back/view/view_copy_data.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import CopyData from '../components/copy_data';
 
+/**
+ * Fungsi untuk menampilkan halaman copy data.
+ * @returns  Hasil View Copy Data menghasilkan copy data.
+ */
 export default function ViewCopyData() {
   return (
     <CopyData/>
