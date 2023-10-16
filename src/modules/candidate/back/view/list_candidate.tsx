1825f40c9ff9ed08576e220c2facd46745b1e47a
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-16 16:35:16 +0800

    style: update candidate
    Deskripsi:
    - update view candidate
    
    No Issue

--- src/modules/candidate/back/view/list_candidate.tsx
+++ src/modules/candidate/back/view/list_candidate.tsx
@@ -4,6 +4,8 @@ import { Box, Button, Center, Group, Paper, Select, SimpleGrid, Stack, Text, Uns
 import { useState } from "react"
 import TableCandidate from "../component/table_candidate"
 import { useRouter } from "next/navigation"
+import { MasterProvinceGetAll } from "@/modules/_global/fun/master_province_get_all"
+import { MasterKabGetByProvince } from "@/modules/_global/fun/master_kabupaten_get_by_province"
 
 
 /**
@@ -12,21 +14,48 @@ import { useRouter } from "next/navigation"
  * @returns {component} Select wilayah.
  */
 
-export default function ListCandidates({ title }: { title: string }) {
+export default function ListCandidates({ title, provinsi }: { title: string, provinsi: any }) {
     const router = useRouter()
+
+    const [isProvinsi, setProvinsi] = useState(provinsi)
+    const [isKab, setKab] = useState<any>([])
+
+    async function onKabupaten({ idProv }: { idProv: any }) {
+
+        const dataKab = await MasterKabGetByProvince({ idProvinsi: Number(idProv) })
+        setKab(dataKab)
+    }
 
     return (
         <>
+
+            {/* {JSON.stringify(isProvinsi)} */}
             <Stack>
                 <Text fw={"bold"}>CANDIDATE</Text>
             </Stack>
             <Group grow mt={30}>
                 <Select
                     placeholder="PROVINCE"
-                    data={["BALI", "JAWA BARAT", "JAWA TIMUR", "KALIMANTAN TENGAH"]}
+                    data={isProvinsi.map((pro: any) => ({
+                        value: String(pro.id),
+                        label: pro.name
+                    }))}
+                    searchable
+                    label="Provinsi"
+                    required
+                    onChange={(val) => {
+                        onKabupaten({ idProv: val })
+                    }}
                 />
-                <Select placeholder="CITY" data={["BADUNG", "DENPASAR", "TABANAN"]} />
-                <Button bg={"gray"} onClick={() => router.push('candidate?prov=bali')}>
+                <Select searchable
+                    label="Kabupaten"
+                    placeholder="CITY"
+                    data={isKab.map((kab: any) => ({
+                        value: String(kab.id),
+                        label: kab.name
+                    }))}
+                />
+                <Button mt={25} bg={"gray"} onClick={() => router.push('candidate?prov=bali')}>
                     PROCCESS
                 </Button>
             </Group>
