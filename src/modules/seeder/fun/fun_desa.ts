812d0236f08813b1de3abaeb230d6c68f743b397
amel <amel@amel.local>
2023-10-16 16:24:41 +0800

    feature: update data seeder
    Deskripsi:
    - Update data provinsi terbaru
    - Update data seeder kabkot terbaru
    - Update data seeder kecamatan terbaru
    - Update data seeder kelurahan terbaru
    
    No Issues

--- src/modules/seeder/fun/fun_desa.ts
+++ src/modules/seeder/fun/fun_desa.ts
@@ -4,59 +4,59 @@ import prisma from "@/modules/_global/bin/prisma"
 import { seederDesa1, seederDesa2, seederDesa3 } from ".."
 
 export async function funSeederDesa() {
-    for (let data of seederDesa1){
+    for (let data of seederDesa1) {
         await prisma.areaKelurahan.upsert({
             where: {
                 id: Number(data.id)
             },
             create: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             },
             update: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             }
         })
     }
-    for (let data of seederDesa2){
+    for (let data of seederDesa2) {
         await prisma.areaKelurahan.upsert({
             where: {
                 id: Number(data.id)
             },
             create: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             },
             update: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             }
         })
     }
-    for (let data of seederDesa3){
+    for (let data of seederDesa3) {
         await prisma.areaKelurahan.upsert({
             where: {
                 id: Number(data.id)
             },
             create: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             },
             update: {
                 id: Number(data.id),
-                idKecamatan: Number(data.masterKecamatanId),
+                idKecamatan: Number(data.idKecamatan),
                 name: data.name
             }
         })
     }
     return {
-        success : true,
+        success: true,
         message: "Success Desa / kelurahan"
     }
 }
--- src/modules/seeder/index.ts
+++ src/modules/seeder/index.ts
@@ -1,10 +1,10 @@
 import SeederView from "./view/seeder_view";
-import seederProvinsi from "./data/provinsi.json"
-import seederKabupaten from "./data/kabupaten.json"
-import seederKecamatan from "./data/kecamatan.json"
-import seederDesa1 from "./data/desa_1.json"
-import seederDesa2 from "./data/desa_2.json"
-import seederDesa3 from "./data/desa_3.json"
+import seederProvinsi from "./data/area_provinsi.json"
+import seederKabupaten from "./data/area_kabkot.json"
+import seederKecamatan from "./data/area_kecamatan.json"
+import seederDesa1 from "./data/area_desa_1.json"
+import seederDesa2 from "./data/area_desa_2.json"
+import seederDesa3 from "./data/area_desa_3.json"
 import seederLeader from "./data/category_leader_trait_assessment.json"
 import seederPublic from "./data/category_public_concern_trend.json"
 import seederComponents from "./data/component.json"
@@ -13,15 +13,15 @@ import seederUserAccess from "./data/user_access.json"
 import seederUser from "./data/user.json"
 
 export { SeederView }
-export {seederProvinsi}
-export {seederKabupaten}
-export {seederKecamatan}
-export {seederDesa1}
-export {seederDesa2}
-export {seederDesa3}
-export {seederLeader}
-export {seederPublic}
-export {seederComponents}
-export {seederUserRole}
-export {seederUserAccess}
-export {seederUser}
+export { seederProvinsi }
+export { seederKabupaten }
+export { seederKecamatan }
+export { seederDesa1 }
+export { seederDesa2 }
+export { seederDesa3 }
+export { seederLeader }
+export { seederPublic }
+export { seederComponents }
+export { seederUserRole }
+export { seederUserAccess }
+export { seederUser }
