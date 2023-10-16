a6c45cfbbc6da5ecacef5bded5151d9233070a7b
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-16 16:41:31 +0800

    feat : update dashboard
    Deskripsi:
    - update page candidate
    
    No Issue

--- src/app/dashboard/(admin)/candidate/page.tsx
+++ src/app/dashboard/(admin)/candidate/page.tsx
@@ -1,12 +1,15 @@
+import { MasterKabGetByProvince } from '@/modules/_global/fun/master_kabupaten_get_by_province';
+import { MasterProvinceGetAll } from '@/modules/_global/fun/master_province_get_all';
 import { ListCandidates } from '@/modules/candidate';
 import React from 'react';
 
-function Page({searchParams}:{searchParams:{prov:string}}) {
+export default async function Page({ searchParams, idProvinsi }: { searchParams: { prov: string }, idProvinsi: any }) {
+  const data = await MasterProvinceGetAll()
+  // const kab = await MasterKabGetByProvince({idProvinsi:0})
   return (
     <>
-      <ListCandidates title={searchParams.prov}/>
+      <ListCandidates title={searchParams.prov} provinsi={data} />
     </>
   );
 }
 
-export default Page;
