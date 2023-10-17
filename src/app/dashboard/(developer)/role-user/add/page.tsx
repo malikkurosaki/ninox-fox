07aceee3f6b99d8a9fa4c90478cf278cb7ab8525
lukman <lukman@lukmans-MacBook-Air.local>
2023-10-17 14:19:21 +0800

    style: tambah dashboard role user
    Deskripsi:
    - tambah page role user
    - tambah add role user
    - tambah edit role user
    
    No Issue

---
+++ src/app/dashboard/(developer)/role-user/add/page.tsx
@@ -0,0 +1,12 @@
+import { AddRoleUser } from '@/modules/role_user';
+import React from 'react';
+
+function Page() {
+  return (
+    <>
+      <AddRoleUser />
+    </>
+  );
+}
+
+export default Page;
