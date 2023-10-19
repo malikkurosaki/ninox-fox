958513f37ab786489d775df001c5669dd666ed97
amel <amel@amel.local>
2023-10-19 13:41:34 +0800

    feat : pencarian by area halaman region hot issue
    Deskripsi:-
    - masang search params
    - backend data region by area
    
    No Issues

--- src/modules/region_hot_issue/back/view/view_list_region.tsx
+++ src/modules/region_hot_issue/back/view/view_list_region.tsx
@@ -1,6 +1,6 @@
 
 "use client"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -14,14 +14,57 @@ import {
 import { useRouter } from "next/navigation";
 import UploadRegion from "../components/upload_region";
 import TableRegion from "../components/table_region";
+import { MasterKabGetByProvince, MasterKecGetByKab } from "@/modules/_global";
+import toast from "react-simple-toasts";
+import _ from "lodash";
 
 /**
  * Fungsi untuk menampilkan table list region.
  * @param {string} title - Judul table.
  * @returns {component} Table list region sesuai dengan parameter.
  */
-export default function ViewListRegion({ title }: { title: string }) {
+
+
+export default function ViewListRegion({ param, provinsi, kabupaten, kecamatan, datatable }: { param: any, provinsi: any, kabupaten: any, kecamatan: any, datatable: any }) {
   const router = useRouter()
+
+  const [dataProvinsi, setDataProvinsi] = useState(provinsi)
+  const [dataKabupaten, setDataKabupaten] = useState<any>(kabupaten)
+  const [dataKecamatan, setDataKecamatan] = useState<any>(kecamatan)
+  const [isProvinsi, setProvinsi] = useState<any>(param.idProvinsi || null)
+  const [isKabupaten, setKabupaten] = useState<any>(param.idKabkot || null)
+  const [isKecamatan, setKecamatan] = useState<any>(param.idKec || null)
+
+
+  useEffect(() => {
+    setProvinsi((param.idProvinsi == 0) ? null : param.idProvinsi)
+    setKabupaten((param.idKabkot == 0) ? null : param.idKabkot)
+    setKecamatan((param.idKec == 0) ? null : param.idKec)
+  }, [param])
+
+
+  async function onProvinsi({ idProv }: { idProv: any }) {
+    setProvinsi(idProv)
+    setKabupaten(null)
+    setKecamatan(null)
+    const dataDbKab = await MasterKabGetByProvince({ idProvinsi: Number(idProv) })
+    setDataKabupaten(dataDbKab)
+    setDataKecamatan([])
+  }
+
+
+  async function onKabupaten({ idKab }: { idKab: any }) {
+    setKabupaten(idKab)
+    setKecamatan(null)
+    const dataDbKec = await MasterKecGetByKab({ idKabkot: idKab })
+    setDataKecamatan(dataDbKec)
+  }
+
+  function onProccess() {
+    if (_.isNull(isProvinsi)) return toast("Silahkan pilih provinsi", { theme: "dark" })
+    router.replace('/dashboard/region-hot-issue?prov=' + isProvinsi + '&city=' + isKabupaten + '&kec=' + isKecamatan)
+  }
+
   return (
     <>
       <Stack>
@@ -37,27 +80,41 @@ export default function ViewListRegion({ title }: { title: string }) {
               <Stack>
                 <Select
                   placeholder="PROVINCE"
-                  data={[
-                    "BALI",
-                    "JAWA BARAT",
-                    "JAWA TIMUR",
-                    "KALIMANTAN TENGAH",
-                  ]}
+                  data={dataProvinsi.map((pro: any) => ({
+                    value: String(pro.id),
+                    label: pro.name
+                  }))}
+                  value={isProvinsi}
                   required
                   label={"Provinsi"}
                   searchable
+                  onChange={(val) => onProvinsi({ idProv: val })}
                 />
                 <Select
                   placeholder="CITY"
-                  data={["BADUNG", "DENPASAR", "TABANAN"]}
+                  data={dataKabupaten.map((kab: any) => ({
+                    value: String(kab.id),
+                    label: kab.name
+                  }))}
+                  value={isKabupaten}
+                  label="Kabupaten/Kota"
+                  searchable
+                  onChange={(val) => onKabupaten({ idKab: val })}
                 />
                 <Select
                   placeholder="SUBDISTRICT"
-                  data={["KUTA SELATAN ", "KUTA UTARA", "MENGWI"]}
+                  data={dataKecamatan.map((kec: any) => ({
+                    value: String(kec.id),
+                    label: kec.name
+                  }))}
+                  value={isKecamatan}
+                  label="Kecamatan"
+                  searchable
+                  onChange={(val) => setKecamatan(val)}
                 />
                 <Button
                   bg={"gray"}
-                  onClick={() => router.push('region-hot-issue?prov=bali')}
+                  onClick={() => onProccess()}
                 >
                   PROCCESS
                 </Button>
@@ -89,9 +146,9 @@ export default function ViewListRegion({ title }: { title: string }) {
           </Group>
         </SimpleGrid>
       </Box>
-      {title && (
+      {!_.isNull(datatable.title) && (
         <Box pt={30}>
-          <TableRegion />
+          <TableRegion data={datatable.data} title={datatable.title} />
         </Box>
       )}
     </>
--- src/modules/region_hot_issue/index.ts
+++ src/modules/region_hot_issue/index.ts
@@ -1,4 +1,6 @@
+import funGetRhiByArea from "./back/fun/get_rhi_by_area";
 import ViewListRegion from "./back/view/view_list_region";
 
 
-export {ViewListRegion}
+export { ViewListRegion }
+export { funGetRhiByArea }
