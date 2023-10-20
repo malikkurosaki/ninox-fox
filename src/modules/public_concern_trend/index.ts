e1b255a37ff30a57d3ebf3609532194f131d6e5d
amel <amel@amel.local>
2023-10-20 17:32:13 +0800

    feat: download csv pct
    Deskripsi:
    - update searchparam
    - membuat fungsi ambil data untuk download pada halaman pce
    
    No Issues

--- src/modules/public_concern_trend/index.ts
+++ src/modules/public_concern_trend/index.ts
@@ -1,6 +1,8 @@
+import funDownloadPCT from "./back/fun/download_pct_by_area"
 import funGetPctByArea from "./back/fun/get_pct_by_area"
 import ViewPublic from "./back/view/view_public"
 
 
 export { ViewPublic }
 export { funGetPctByArea }
+export { funDownloadPCT }
