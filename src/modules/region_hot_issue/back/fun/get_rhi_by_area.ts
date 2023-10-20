010646aa6ea6b9b4941012748f515c1ce63eab19
amel <amel@amel.local>
2023-10-20 10:12:48 +0800

    feat : backend lta dan rhi
    Deskripsi :
    - backend ambil data pada halaman lta
    - backend ambil data pada halaman rhi
    - membuat seeder value lta
    - membuat seeder value rhi
    
    No Issues

--- src/modules/region_hot_issue/back/fun/get_rhi_by_area.ts
+++ src/modules/region_hot_issue/back/fun/get_rhi_by_area.ts
@@ -2,6 +2,7 @@
 
 import { provinsiCount } from "@/modules/_global";
 import prisma from "@/modules/_global/bin/prisma";
+import _ from "lodash"
 
 /**
  * Fungsi untuk menampilkan data audience berdasarkan area
@@ -11,47 +12,118 @@ import prisma from "@/modules/_global/bin/prisma";
 
 
 export default async function funGetRhiByArea({ find }: { find: any }) {
-    let titleTrue, dataTable = <any>[], area
+    let titleTrue, dataTable = <any>[], area, th
 
     const nProv = await provinsiCount();
 
     if (find.idProvinsi > 0 && find.idProvinsi <= nProv) {
         if (find.idKec == 0 && find.idKabkot == 0) {
-            dataTable = []
+            dataTable = await prisma.regionHotIssues.findMany({
+                where: {
+                    idProvinsi: find.idProvinsi,
+                    NOT: {
+                        idKabkot: null,
+                    },
+                    idKecamatan: null,
+                    idKelurahan: null
+                },
+                select: {
+                    description: true,
+                    AreaKabkot: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+
+            dataTable = dataTable.map((v: any) => ({
+                ..._.omit(v, ["AreaKabkot"]),
+                name: v.AreaKabkot.name
+            }))
+
             area = await prisma.areaProvinsi.findUnique({
                 where: {
                     id: find.idProvinsi
                 }
             })
             titleTrue = "PROVINSI " + area?.name
+            th = "KABUPATEN/KOTA"
 
         } else if (find.idKec == 0 && find.idKabkot > 0) {
-            dataTable = []
+            dataTable = await prisma.regionHotIssues.findMany({
+                where: {
+                    idProvinsi: find.idProvinsi,
+                    idKabkot: find.idKabkot,
+                    NOT: {
+                        idKecamatan: null,
+                    },
+                    idKelurahan: null
+                },
+                select: {
+                    description: true,
+                    AreaKecamatan: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+
+            dataTable = dataTable.map((v: any) => ({
+                ..._.omit(v, ["AreaKecamatan"]),
+                name: v.AreaKecamatan.name
+            }))
             area = await prisma.areaKabkot.findUnique({
                 where: {
                     id: find.idKabkot
                 }
             })
-            titleTrue = "" + area?.name
+            titleTrue = area?.name.toUpperCase()
+            th = "KECAMATAN"
 
         } else {
-            dataTable = []
+            dataTable = await prisma.regionHotIssues.findMany({
+                where: {
+                    idProvinsi: find.idProvinsi,
+                    idKabkot: find.idKabkot,
+                    idKecamatan: find.idKec,
+                    NOT: {
+                        idKelurahan: null
+                    },
+                },
+                select: {
+                    description: true,
+                    AreaKelurahan: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+
+            dataTable = dataTable.map((v: any) => ({
+                ..._.omit(v, ["AreaKelurahan"]),
+                name: v.AreaKelurahan.name
+            }))
             area = await prisma.areaKecamatan.findUnique({
                 where: {
                     id: find.idKec
                 }
             })
             titleTrue = "KECAMATAN " + area?.name.toUpperCase()
-
+            th = "KELURAHAN"
         }
 
     } else {
         titleTrue = null;
         dataTable = [];
+        th = null
     }
 
     const allData = {
         title: titleTrue,
+        thTitle: th,
         data: dataTable
     }
 
