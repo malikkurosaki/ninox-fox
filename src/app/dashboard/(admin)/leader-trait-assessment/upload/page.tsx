81edc780e30805452ae3babaf7b2d3eb5424ff15
amel <amel@amel.local>
2023-12-04 15:31:05 +0800

    upd: lta dashboard admin
    Deskripsi:
    - membuat view upload data lta
    - membuat server action upload data lta
    - membuat modal konfirmasi upload data lta
    - membuat list untuk seluruh indonesia (tanpa memilih provinsi)
    
    No Issues

--- src/app/dashboard/(admin)/leader-trait-assessment/upload/page.tsx
+++ src/app/dashboard/(admin)/leader-trait-assessment/upload/page.tsx
@@ -1,6 +1,4 @@
-
 import { ViewUploadLeader } from '@/modules/leader_trait_assessment';
-import { funUploadLeader } from '@/modules/leader_trait_assessment/back/fun/fun_upload_leader';
 import React from 'react';
 
 export default async function Page() {
--- src/modules/leader_trait_assessment/back/view/upload_leader.tsx
+++ src/modules/leader_trait_assessment/back/view/upload_leader.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { ButtonBack } from '@/modules/_global';
-import { Box, Button, Center, Group, ScrollArea, Stack, Table, Text, Title, rem } from '@mantine/core';
+import { Box, Button, Center, Group, Modal, ScrollArea, Stack, Table, Text, Title, rem } from '@mantine/core';
 import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import papa from 'papaparse'
 import { useState } from 'react';
@@ -8,7 +8,9 @@ import { AiOutlineUpload } from 'react-icons/ai';
 import { GrDocumentCsv } from 'react-icons/gr';
 import { MdCancelPresentation } from 'react-icons/md';
 import toast from 'react-simple-toasts';
-import { funUploadLeader } from '../fun/fun_upload_leader';
+import { useAtom } from 'jotai';
+import { isModalLta } from '../val/val_lta';
+import ModalUploadLta from '../components/modal_upload_lta';
 
 
 /**
@@ -17,15 +19,38 @@ import { funUploadLeader } from '../fun/fun_upload_leader';
  */
 export function ViewUploadLeader() {
   const [json, setJson] = useState<any[]>([])
-  async function onpload(data: any) {
-    const res = await funUploadLeader(data)
-    if (res.success) return toast(res.message)
-  }
-  const [dataUpload, setDataUplaod] = useState()
+  const [openModal, setOpenModal] = useAtom(isModalLta)
 
-  const getHeadings = () => {
-    return json
-  };
+
+  async function onLoad(data: any) {
+    if (data.length > 0) {
+      if (
+        ('id' in data[0]) &&
+        ('Provinsi' in data[0]) &&
+        ('Kabkot' in data[0]) &&
+        ('Kecamatan' in data[0]) &&
+        ('Kelurahan' in data[0]) &&
+        ('pekerjaKeras' in data[0]) &&
+        ('cerdas' in data[0]) &&
+        ('jujur' in data[0]) &&
+        ('merakyat' in data[0]) &&
+        ('tegas' in data[0]) &&
+        ('berpengalamanMemimpin' in data[0]) &&
+        ('berprestasi' in data[0]) &&
+        ('latarBelakangMiliter' in data[0]) &&
+        ('agamis' in data[0])
+      ) {
+        setJson(data as any)
+      } else {
+        setJson([])
+        toast('Format CSV salah', { theme: 'dark' })
+      }
+    } else {
+      setJson([])
+      toast('Data Kosong', { theme: 'dark' })
+    }
+  }
+
 
   return (
     <>
@@ -40,7 +65,7 @@ export function ViewUploadLeader() {
             borderRadius: 10
           }}
         >
-          <Text fw={"bold"} c={"white"} mb={20}>UPLOAD DATA CSV</Text>
+          <Text fw={"bold"} c={"white"} mb={20}>UPLOAD DATA LEADER TRAIT ASSESSMENT</Text>
           <Dropzone
             style={{
               border: "1px dashed",
@@ -51,8 +76,7 @@ export function ViewUploadLeader() {
             onDrop={async (files: any) => {
               const csv_file = Buffer.from(await files[0].arrayBuffer()).toString()
               const { data } = papa.parse(csv_file, { header: true, })
-              setJson(data as any)
-              // console.log('data', data);
+              onLoad(data)
             }}
             onReject={(files: any) => {
               toast("success")
@@ -83,59 +107,124 @@ export function ViewUploadLeader() {
 
               <div>
                 <Text size="xl" inline>
-                  Drag images here or click to select files
+                  Drag files here or click to select files
                 </Text>
                 <Text size="sm" inline mt={7} >
-                  Attach as many files as you like, each file should not exceed 5mb
+                  Attach one file, the file must not exceed 5MB
                 </Text>
               </div>
             </Group>
           </Dropzone>
-          <Box style={{
-            borderRadius: 10,
-            paddingTop: 20
-          }}>
+          {
+            (json.length > 0) && (
+              <>
+                <Box style={{
+                  borderRadius: 10,
+                  paddingTop: 20
+                }}>
+                  <Box
+                    style={{
+                      backgroundColor: "white",
+                      padding: 10,
+                      borderRadius: 10,
+                    }}
+                  >
 
-            <Box
-              style={{
-                backgroundColor: "white",
-                padding: 10,
-                borderRadius: 10,
-              }}
-            >
-              <ScrollArea>
-                <Table
-                  withTableBorder
-                  withRowBorders={false}
-                  horizontalSpacing="xl"
-                >
-                  <Table.Thead>
-                    <Table.Tr
-                      style={{
-                        borderBottom: "1px solid #CED4D9",
-                      }}
-                    >
-                      <Table.Th>No</Table.Th>
-                      <Table.Th>Nama</Table.Th>
-                    </Table.Tr>
-                  </Table.Thead>
-                  <Table.Tbody>
-                    {json.map(home =>
-                      <Table.Tr key={home.id}>
-                        <Table.Td>{home.id}</Table.Td>
-                        <Table.Td>{home.name}</Table.Td>
-                      </Table.Tr>
-                    )}
-                  </Table.Tbody>
-                </Table>
-              </ScrollArea>
-            </Box>
-          </Box>
-          <Group >
-            <Button mt={20} fullWidth bg={"dark"}>UPLOAD</Button>
-          </Group>
+
+                    <ScrollArea>
+                      <Table
+                        withTableBorder
+                        withRowBorders={false}
+                        horizontalSpacing="xl"
+                      >
+                        <Table.Thead>
+                          <Table.Tr
+                            style={{
+                              borderBottom: "1px solid #CED4D9",
+                            }}
+                          >
+                            <Table.Th>ID</Table.Th>
+                            <Table.Th>Provinsi</Table.Th>
+                            <Table.Th>Kabupaten/Kota</Table.Th>
+                            <Table.Th>Kecamatan</Table.Th>
+                            <Table.Th>Kelurahan</Table.Th>
+                            <Table.Th>Pekerja Keras</Table.Th>
+                            <Table.Th>Cerdas</Table.Th>
+                            <Table.Th>Jujur</Table.Th>
+                            <Table.Th>Merakyat</Table.Th>
+                            <Table.Th>Tegas</Table.Th>
+                            <Table.Th>Berpengalaman Memimpin</Table.Th>
+                            <Table.Th>Berprestasi</Table.Th>
+                            <Table.Th>Latar Belakang Militer</Table.Th>
+                            <Table.Th>Agamis</Table.Th>
+                          </Table.Tr>
+                        </Table.Thead>
+                        <Table.Tbody>
+                          {json.map(home =>
+                            <Table.Tr key={home.id}>
+                              <Table.Td>{home.id}</Table.Td>
+                              <Table.Td>{home.Provinsi}</Table.Td>
+                              <Table.Td>{home.Kabkot}</Table.Td>
+                              <Table.Td>{home.Kecamatan}</Table.Td>
+                              <Table.Td>{home.Kelurahan}</Table.Td>
+                              <Table.Td>{home.pekerjaKeras}</Table.Td>
+                              <Table.Td>{home.cerdas}</Table.Td>
+                              <Table.Td>{home.jujur}</Table.Td>
+                              <Table.Td>{home.merakyat}</Table.Td>
+                              <Table.Td>{home.tegas}</Table.Td>
+                              <Table.Td>{home.berpengalamanMemimpin}</Table.Td>
+                              <Table.Td>{home.berprestasi}</Table.Td>
+                              <Table.Td>{home.latarBelakangMiliter}</Table.Td>
+                              <Table.Td>{home.agamis}</Table.Td>
+                            </Table.Tr>
+                          )}
+                        </Table.Tbody>
+                      </Table>
+                    </ScrollArea>
+
+                  </Box>
+                </Box>
+                <Group justify="flex-end">
+                  <Box
+                    style={{
+                      padding: 10,
+                      borderRadius: 5,
+                      paddingLeft: 20,
+                      paddingRight: 20,
+                      position: "fixed",
+                      bottom: 30,
+                      right: 30,
+                      backgroundColor: "green",
+                      boxShadow: "2px solid gray",
+                      cursor: 'pointer'
+                    }}
+                    onClick={() => {
+                      setOpenModal(true)
+                    }}
+                  >
+                    <Group>
+                      <AiOutlineUpload size={25} color={"white"} />
+                      <Text fw={"bold"} c={"white"}>UPLOAD</Text>
+                    </Group>
+                  </Box>
+                </Group>
+              </>
+            )
+          }
         </Box>
       </Stack>
+
+      <Modal
+        opened={openModal}
+        onClose={() => setOpenModal(false)}
+        centered
+        withCloseButton={false}
+        closeOnClickOutside={false}
+      >
+        <ModalUploadLta data={json} onSuccess={(val) => {
+          setJson([])
+        }} />
+      </Modal>
     </>
   );
 }
