1bb642b93a14b84e14f0b8bc4271fd2177ff3cb1
lukman <lukman@lukmans-MacBook-Air.local>
2023-12-05 17:31:30 +0800

    style : update mlai
    Deskripsi:
    - tambah view ml ai
    
    No Issue

--- src/modules/mlai/index.ts
+++ src/modules/mlai/index.ts
@@ -2,8 +2,10 @@ import funGetAllMlAi from "./back/fun/fun_get_all_mlai";
 import AddMlAi from "./back/view/add_mlai";
 import EditMlAi from "./back/view/edit_mlai";
 import ListMlAi from "./back/view/list_mlai";
+import ViewMlAi from "./front/view/view_ml_ai";
 
 export { funGetAllMlAi }
 export { EditMlAi }
 export { AddMlAi }
 export { ListMlAi }
+export {ViewMlAi}
