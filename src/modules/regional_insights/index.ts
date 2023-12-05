0d37a5ba6bce58c76fa1951a9592635828e624fc
lukman <lukman@lukmans-MacBook-Air.local>
2023-12-05 17:35:28 +0800

    style : update regional_insights
    Deskripsi:
    - update echart sentimen analisis
    - tambah view detail regional_insights
    
    NO Issue

--- src/modules/regional_insights/index.ts
+++ src/modules/regional_insights/index.ts
@@ -1,3 +1,5 @@
+import ViewDetailRegionalInsights from "./view/view_detail_regional_insights";
 import ViewRegionalInsights from "./view/view_regional_insights";
 
 export {ViewRegionalInsights}
+export {ViewDetailRegionalInsights}
