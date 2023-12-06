4804ff3cee192661a9ccc4f660fb24a9c2655e7a
amel <amel@amel.local>
2023-12-06 14:38:03 +0800

    upd : tipe data pada fungsi get one candidate

--- src/modules/candidate/back/fun/get_one_candidate.ts
+++ src/modules/candidate/back/fun/get_one_candidate.ts
@@ -5,7 +5,7 @@ import prisma from "@/modules/_global/bin/prisma"
 export default async function funGetOneCandidate({ id }: { id: any }) {
     const data = await prisma.candidate.findUnique({
         where: {
-            id: id
+            id: String(id)
         }
     })
 
