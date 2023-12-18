22ca489659c8947d06c3ec5d59de792c1512c9af
lukman <lukman@lukmans-MacBook-Air.local>
2023-12-18 17:08:56 +0800

    feat : update public_concern_trend
    Deskripsi:
    - update index
    
    No Issue

--- src/modules/public_concern_trend/index.ts
+++ src/modules/public_concern_trend/index.ts
@@ -1,3 +1,5 @@
+import ModalUploadPct from "./back/components/modal_upload_public"
+import TablePublic from "./back/components/table_public"
 import funDownloadPCT from "./back/fun/download_pct_by_area"
 import funGetPctByArea from "./back/fun/get_pct_by_area"
 import ViewPublic from "./back/view/view_public"
@@ -8,3 +10,5 @@ export { ViewPublic }
 export { funGetPctByArea }
 export { funDownloadPCT }
 export { ViewUploadPct }
+export { ModalUploadPct }
+export { TablePublic }
