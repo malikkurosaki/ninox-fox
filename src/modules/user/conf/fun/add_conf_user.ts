8fef4d7f49553b9848a4990ec0a7067d0092b4da
lukman <lukman@lukmans-MacBook-Air.local>
2023-12-21 17:50:43 +0800

    feat : update user
    deskripsi:
    - update user
    
    No Issue

--- src/modules/user/conf/fun/add_conf_user.ts
+++ src/modules/user/conf/fun/add_conf_user.ts
@@ -1,8 +1,10 @@
 "use server"
 
+import { MasterKabGetByProvince } from "@/modules/_global"
 import prisma from "@/modules/_global/bin/prisma"
+import _ from "lodash"
 
-export default async function funAddConfUser({data, dataArea, isFront}: {data: any, dataArea: any, isFront: any}) {
+export default async function funAddConfUser({ data, dataArea, isFront }: { data: any, dataArea: any, isFront: any }) {
     const user = await prisma.user.create({
         data: {
             idUserRole: Number(data.idUserRole),
@@ -17,12 +19,35 @@ export default async function funAddConfUser({data, dataArea, isFront}: {data: a
         }
     })
 
-    for (let i of dataArea) {
-        await prisma.userArea.create({
-            data:{
+    if (!data.isAllArea) {
+        for (let i of dataArea) {
+            await prisma.userArea.create({
+                data: {
+                    idUser: user.id,
+                    idProvinsi: Number(i),
+                    // isFront: (isFront == Number(i) ? true : false)
+                    isFront: (i == 1 ? true : false)
+                }
+            })
+            const kab = await MasterKabGetByProvince({ idProvinsi: Number(i) })
+            const wilayahTrue = kab.map((v: any) => ({
+                ..._.omit(v, ["id", "idProvinsi", "name", "isActive", "createdAt", "updatedAt"]),
                 idUser: user.id,
-                idProvinsi: Number(i),
-                isFront: (isFront == Number(i) ? true : false)
+                idProvinsi: v.idProvinsi,
+                idKabkot: v.id,
+                isFront: false
+            }));
+
+            await prisma.userArea.createMany({
+                data: wilayahTrue
+            })
+        }
+    } else {
+        await prisma.userArea.create({
+            data: {
+                idUser: user.id,
+                idProvinsi: Number(1),
+                isFront: true
             }
         })
     }
