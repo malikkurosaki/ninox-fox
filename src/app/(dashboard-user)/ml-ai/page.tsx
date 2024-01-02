16a7214b894d593048fbed2f9b200b1bb9c744ee
lukman <lukman@lukmans-MacBook-Air.local>
2024-01-02 17:52:21 +0800

    feat : update app

--- src/app/(dashboard-user)/ml-ai/page.tsx
+++ src/app/(dashboard-user)/ml-ai/page.tsx
@@ -1,10 +1,14 @@
-import { ViewMlAi } from '@/modules/mlai';
+import { funGetOneCandidateFront } from '@/modules/candidate';
+import funGetAllCandidateFront from '@/modules/candidate/front/fun/get_all_candidate_front';
+import { ViewMlAi, funGetMlAiFront } from '@/modules/mlai';
 import React from 'react';
 
-function Page() {
+export default async function Page() {
+const data = await funGetMlAiFront({})
+const can = await funGetAllCandidateFront()
+const oneCandidate = await funGetOneCandidateFront({candidate: "clqdehlfg000d3vyyx3t1uzyh"})
   return (
-    <ViewMlAi/>
+    <ViewMlAi data={data} candidate={can} oneCandidate={oneCandidate}/>
   );
 }
 
-export default Page;
