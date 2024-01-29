9c63b3bf6c487a08eb41140b3533f3482a35c1b8
amel <amel@amel.local>
2024-01-29 14:55:41 +0800

    qc : dashboard user
    Deskripsi:
    - menampilkan data kandidate sesuai dengan kandidate default
    - nb: sebelum nya merupakan urutan nama kandidat
    
    No Issues

--- src/app/(dashboard-user)/step/page.tsx
+++ src/app/(dashboard-user)/step/page.tsx
@@ -1,11 +1,12 @@
-import { funGetAllCandidateFront, funGetOneCandidateFront } from '@/modules/candidate';
+import { funGetAllCandidateFront, funGetOneCandidateFront, funGetUserDefaultFront } from '@/modules/candidate';
 import { ViewStep, funGetStepFront } from '@/modules/step';
 import React from 'react';
 
 export default async function Page() {
-  const data = await funGetStepFront({})
+  const canDef = await funGetUserDefaultFront()
+  const data = await funGetStepFront({ candidate: canDef.idCandidate })
   const can = await funGetAllCandidateFront()
-  const oneCandidate = await funGetOneCandidateFront({})
+  const oneCandidate = await funGetOneCandidateFront({ candidate: canDef.idCandidate })
 
   return (
     <ViewStep data={data} candidate={can} oneCandidate={oneCandidate} />
