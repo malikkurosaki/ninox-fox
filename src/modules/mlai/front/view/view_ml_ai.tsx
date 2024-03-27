24ffde6b8998ce44084e52e1bc58aaa9d93802ee
lukman <lukman@lukmans-MacBook-Air.local>
2024-03-27 10:49:07 +0800

    style : ml Ai

--- src/modules/mlai/front/view/view_ml_ai.tsx
+++ src/modules/mlai/front/view/view_ml_ai.tsx
@@ -1,13 +1,14 @@
 "use client"
 import { PageSubTitle } from '@/modules/_global';
 import { funGetOneCandidateFront } from '@/modules/candidate';
-import { Box, Button, Divider, Group, Image, Indicator, Menu, MenuTarget, ScrollArea, Select, Stack, Text } from '@mantine/core';
+import { ActionIcon, Box, Button, Container, Divider, Group, Image, Indicator, Menu, MenuTarget, ScrollArea, Select, Stack, Text } from '@mantine/core';
 import React, { useState } from 'react';
 import TextAnimation from 'react-typing-dynamics';
 import { funGetMlAiFront } from '../..';
 import _ from 'lodash';
-import { DatePicker, DatePickerProps } from '@mantine/dates';
+import { DateInput, DatePicker, DatePickerProps } from '@mantine/dates';
 import moment from 'moment';
+import { CiMenuKebab } from 'react-icons/ci';
 
 const dataMl = [
   {
@@ -35,7 +36,7 @@ export default function ViewMlAi({ data, candidate, oneCandidate }: { data: any,
     setImgCan(`/img/candidate/${dataCan?.img}`)
   }
   const dayRenderer: DatePickerProps['renderDay'] = (date) => {
-    
+
     const coba = moment(date).format('DD-MM-YYYY')
     const day = date.getDate()
     const muncul = randomInt.includes(coba)
@@ -52,18 +53,31 @@ export default function ViewMlAi({ data, candidate, oneCandidate }: { data: any,
 
     return dateStrings.join(' ');
   }
+  const [value, setValue] = useState<Date | null>(null);
 
   return (
     <>
       {/* <PageSubTitle text1='ML-AI' text2='PROMPT RECOMENDATIONS' /> */}
       <PageSubTitle text1='REKOMENDASI CEPAT' text2='ML-AI' />
-      <Stack pt={20}>
+      <Box pt={20}>
         <Box
         >
           <Image src={isImgCan} bg={"white"} style={{ border: "4px solid white" }} radius={"100%"} alt='Kandidat' maw={200} mx="auto" />
-          <Text fw={"bold"} ta={"center"} c={"white"}>{isNameCan}</Text>
-        </Box>
+          <Container px={0} size="15rem" mt={20}>
+            <Select
 
+              placeholder="Kandidat"
+              data={listCandidate.map((can: any) => ({
+                value: String(can.id),
+                label: can.name
+              }))}
+              value={isCandidate}
+              onChange={(val) => chooseCandidate(val)}
+            />
+          </Container>
+          {/* <Text fw={"bold"} ta={"center"} c={"white"}>{isNameCan}</Text> */}
+        </Box>
+        <Text mt={20} c={"green"} fz={20} fw={"bold"}>PENINGKATAN ANALISIS KEKUATAN</Text>
         <Box pt={20}>
           <Box
             style={{
@@ -72,56 +86,39 @@ export default function ViewMlAi({ data, candidate, oneCandidate }: { data: any,
               borderRadius: 10
             }}
           >
-            <Group justify='space-between'>
-              <Text c={"green"} fz={20} fw={"bold"}>PENINGKATAN ANALISIS KEKUATAN</Text>
-              <Group mt={10}>
-                <Menu position="left-start">
-                  <Menu.Target>
-                    <Button
-                      variant="outline"
-                      style={{
-                        backgroundColor: 'white'
-                      }}
-                    >
-                      <Text c={'#000000'}>Date</Text>
-                    </Button>
-                  </Menu.Target>
-                  <Menu.Dropdown p={20}>
-                    <DatePicker renderDay={dayRenderer} />
-                    <Group justify="space-between" mt={5} >
-                      <Button
-                        w={100}
-                        p={10}
-                        variant="outline"
-                        style={{
-                          backgroundColor: 'white'
-                        }}
-                      >
-                        BATAL
-                      </Button>
-                      <Button
-                        w={100}
-                        variant="filled"
-                      >
-                        OK
-                      </Button>
-                    </Group>
-                  </Menu.Dropdown>
-                </Menu>
-                <Select
-
-                  placeholder="Kandidat"
-                  data={listCandidate.map((can: any) => ({
-                    value: String(can.id),
-                    label: can.name
-                  }))}
-                  value={isCandidate}
-                  onChange={(val) => chooseCandidate(val)}
-                />
-
-              </Group>
-            </Group>
-            <Box pt={20}>
+            <Box>
+              <Box>
+                <Group >
+                  <DateInput
+                    variant="filled"
+                    placeholder="SELECT DATE"
+                    maxDate={new Date()}
+                    minDate={new Date('2023-09-01')}
+                    value={value}
+                    onChange={setValue}
+                    renderDay={dayRenderer}
+                  />
+                  <Button color="indigo" w={100}>08.33</Button>
+                  <Button color="indigo" w={100}>10.00</Button>
+                  <Button color="indigo" w={100}>13.10</Button>
+                  <Button color="indigo" w={100}>15.40</Button>
+                  <Button color="indigo" w={100}>20.20</Button>
+                  <Group>
+                    <Menu shadow="md" width={200} position="right-start">
+                      <Menu.Target>
+                        <ActionIcon variant="subtle" color="rgba(255, 255, 255, 1)" aria-label="Settings">
+                          <CiMenuKebab style={{ width: '70%', height: '70%' }} />
+                        </ActionIcon>
+                      </Menu.Target>
+                      <Menu.Dropdown pb={0}>
+                        <Button color="indigo" mb={5} fullWidth>13.10</Button>
+                        <Button color="indigo" mb={5} fullWidth>15.40</Button>
+                        <Button color="indigo" mb={5} fullWidth>20.20</Button>
+                      </Menu.Dropdown>
+                    </Menu>
+                  </Group>
+                </Group>
+              </Box>
               <ScrollArea h={"34vh"}>
                 <Stack >
                   {
@@ -146,7 +143,7 @@ export default function ViewMlAi({ data, candidate, oneCandidate }: { data: any,
             </Box>
           </Box>
         </Box>
-      </Stack>
+      </Box>
     </>
   );
 }
