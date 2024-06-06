19b721ab89e7aa938a94291a6aaa9abe2e87409e
lukman <lukman@lukmans-MacBook-Air.local>
2024-06-06 12:27:05 +0800

    style : update regional insights
    Deskripsi :
    - update view regional insights
    
    No issue"

--- src/modules/regional_insights/index.ts
+++ src/modules/regional_insights/index.ts
@@ -4,6 +4,7 @@ import funGetEmotionDetailRegional from "./fun/get_emotion_detail_regional";
 import funGetEmotionDetailRegionalNew from "./fun/get_emotion_detail_regional_new";
 import funGetEmotionRegionalInsight from "./fun/get_emotion_regional_insight";
 import funGetEmotionRegionalInsightNew from "./fun/get_emotion_regional_insight_new";
+import { isDetactionNavbar } from "./val/isDetectionNavbar";
 import ViewDetailRegionalInsights from "./view/view_detail_regional_insights";
 import ViewRegionalInsights from "./view/view_regional_insights";
 
@@ -15,3 +16,4 @@ export { funGetEmotionDetailRegional }
 export { funGetAreaDetailRegional }
 export { funGetEmotionRegionalInsightNew }
 export { funGetEmotionDetailRegionalNew }
+export { isDetactionNavbar }
