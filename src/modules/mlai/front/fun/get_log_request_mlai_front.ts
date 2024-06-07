bacf9c5cad4d50de610828def8a573dd925d2ab0
amel <amel@amel.local>
2024-06-07 15:05:42 +0800

    fix : scroll load log request
    Deskripsi:
    - update scrooll load pada halaman log request saat klik send
    
    No Issues

--- src/modules/mlai/front/fun/get_log_request_mlai_front.ts
+++ src/modules/mlai/front/fun/get_log_request_mlai_front.ts
@@ -3,12 +3,12 @@ import prisma from '@/modules/_global/bin/prisma';
 import { funGetUserByCookies } from '@/modules/auth';
 
 // todo: belom default wilayah
-export default async function funGetLogRequestMlaiFront() {
+export default async function funGetLogRequestMlaiFront({page}:{page:any}) {
    const user = await funGetUserByCookies()
-   // const dataTake = page * 15
+   const dataTake = page * 15
    const data = await prisma.mlAiRequest.findMany({
       skip: 0,
-      take: 15,
+      take: dataTake,
       where: {
          idUser: user?.id
       },
