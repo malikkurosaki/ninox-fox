5b0d6fb1957592dfdf77cb17fbce5f3b73c4dfc8
lukman <lukman@lukmans-MacBook-Air.local>
2024-06-10 13:35:09 +0800

    feat : update ml ai
    Deskripsi:
    - update view ml ai data learner 3
    
    No issue

--- src/app/(dashboard-user)/data-learner/page.tsx
+++ src/app/(dashboard-user)/data-learner/page.tsx
@@ -1,10 +1,11 @@
-import { ViewDataLearner, ViewDataLearner2, funGetLogRequestMlaiFront } from '@/modules/mlai';
+import { ViewDataLearner, ViewDataLearner2, ViewDataLearner3, funGetLogRequestMlaiFront } from '@/modules/mlai';
 import React from 'react';
 
 export default async function Page() {
   return (
     // <ViewDataLearner />
-    <ViewDataLearner2 />
+    // <ViewDataLearner2 />
+    <ViewDataLearner3 />
     // <Coba />
   );
 }
