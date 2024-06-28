d0ec6c3e3c026247985322beb64541c29d63a5cb
amel <amel@amel.local>
2024-06-28 09:04:22 +0800

    upd : fix

--- src/app/dashboard/(admin)/emotion/page.tsx
+++ src/app/dashboard/(admin)/emotion/page.tsx
@@ -3,27 +3,25 @@ import { funGetCandidateActiveByArea, funGetUserDefaultFront } from "@/modules/c
 import { ViewBackEmotion, funDownloadEmotion, funGetEmotionByCandidateAreaDate } from "@/modules/emotion";
 import _ from "lodash";
 import moment from "moment";
-import { IoToday } from "react-icons/io5";
 
 export default async function Page({ searchParams }: { searchParams: { prov: any, city: any, can: any, date: any } }) {
   const defaultValue = await funGetUserDefaultFront()
   const kabVal = (_.isNull(defaultValue.idKabkot) || _.isUndefined(defaultValue.idKabkot)) ? 0 : defaultValue.idKabkot
   const today = moment(new Date()).format('YYYY-MM-DD')
-  console.log(defaultValue, kabVal, today)
-  const findData = {
-    idProvinsi: (_.isNaN(Number(searchParams.prov)) ? 0 : Number(searchParams.prov)),
-    idKabkot: (_.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
-    tingkat: (_.isNaN(Number(searchParams.city)) ? 1 : 2),
-    idCandidate: (_.isUndefined(searchParams.can) ? 0 : searchParams.can),
-    date: (_.isUndefined(searchParams.date) ? null : searchParams.date)
-  }
   // const findData = {
-  //   idProvinsi: (_.isNaN(Number(searchParams.prov)) ? defaultValue.idProvinsi : Number(searchParams.prov)),
-  //   idKabkot: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? kabVal : _.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
-  //   tingkat: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? defaultValue.tingkat : _.isNaN(Number(searchParams.city)) ? 1 : 2),
-  //   idCandidate: (_.isUndefined(searchParams.can) ? defaultValue.idCandidate : searchParams.can),
-  //   date: (_.isUndefined(searchParams.date) ? today : searchParams.date)
+  //   idProvinsi: (_.isNaN(Number(searchParams.prov)) ? 0 : Number(searchParams.prov)),
+  //   idKabkot: (_.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
+  //   tingkat: (_.isNaN(Number(searchParams.city)) ? 1 : 2),
+  //   idCandidate: (_.isUndefined(searchParams.can) ? 0 : searchParams.can),
+  //   date: (_.isUndefined(searchParams.date) ? null : searchParams.date)
   // }
+  const findData = {
+    idProvinsi: (_.isNaN(Number(searchParams.prov)) ? defaultValue.idProvinsi : Number(searchParams.prov)),
+    idKabkot: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? kabVal : _.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
+    tingkat: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? defaultValue.tingkat : _.isNaN(Number(searchParams.city)) ? 1 : 2),
+    idCandidate: (_.isUndefined(searchParams.can) ? defaultValue.idCandidate : searchParams.can),
+    date: (_.isUndefined(searchParams.date) ? today : searchParams.date)
+  }
 
 
   const prov = await MasterProvinceGetAll()
